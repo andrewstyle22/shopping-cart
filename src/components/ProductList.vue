d600ec5a56d250442093e6047735664e1c1a8881
andrewstyle22 <andrewstyle22@yahoo.es>
2019-01-14 23:09:53 +0000

    version inicial

---
+++ src/components/ProductList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <img
+      v-if="loading"
+      src="https://i.imgur.com/JfPpwOA.gif" alt="">
+    <ul v-else>
+      <li v-for="(product, index) in products" :key="index">
+        {{ product.title }} - {{ product.price}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// import store from './store/index.js'; lo quitamos aquí y lo agregamos en main.js
+// ahora es globar y reemplazamos store por this.$store
+//import shop from '@/api/shop';  // al agregar store.dispatch('fetchProducts');
+                                      // no necesitamos importar la tienda en el
+                                      // componente
+
+
+export default {
+  // dado que los productos (products) están ahora almacenados globalmente
+  // dentro del estado, por lo tando no necesitamos en el data,
+  // como solo tenemos un solo elementos comentamos todo el data.
+  // lo que necesitamos es una propiedad computada que retornará los
+  // productos desde el estado
+  data() {
+    return {
+      loading: false,
+    }
+  },
+  computed: {
+    products() {
+      // return store.state.products;
+      return this.$store.getters.availableProducts;
+    }
+  },
+  created() { // Aquí solíamos actualizar la propiedad local de datos de products
+              // pero ahora queremos actualizar los productos en el estado
+              // Podrías pensar que en el lugar de this.products podemos usar
+              // store.state.products = products y mientras que esto puede funcionar,
+              // nunca deberías actualizar el estado directamente sin llamar a una
+              // mutación
+    // shop.getProducts(products => {
+    //   //this.products = products;
+    //   // store.state.products = products; es mejor llamar a una mutación
+    //   // es decir, hacer commit de una mutación pasándole le nombre de la mutación,
+    //   // la carga de los productos retornados
+    //   store.commit('setProducts', products);
+
+    // });
+    // vamos a mover este código a action de esta manera no hay que repetir esta
+    // función en todos los archivos que se esté utilizando products
+    //------
+    // Para activar una acción llamaremos al método store.dispatch
+    // primer parámetro nombre de la acción y la segunda es el payload
+    // store.dispatch('fetchProducts', 'toy') => pero se podría pasar una consulta del
+    // tipo de productos a ubicar, por ejemplo  'toy'
+    this.loading = true;
+    this.$store.dispatch('fetchProducts').then(() => this.loading = false);
+    // De esta manera desacoplamos los componentes de la lógica de la API
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/store/index.js
@@ -0,0 +1,91 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+import shop from '@/api/shop';
+
+Vue.use(Vuex);
+
+// Los stores de Vuex tienen cinco opciones
+// State, mutations, getters, actions y modules
+export default new Vuex.Store({
+  state: { // = data
+    products: [],
+    // {id, quantity } Solo almacenaremos objetos que contengan el
+    // código y el número de items que el usuario desee comprar
+    // necesitaremos una acción y una mutación para añadir el producto al carrito
+    cart: [],
+  },
+  getters: { // = computed properties global
+    // por ejemplo queremos mostrar los productos que no están agotados
+    // o calcular el total del carrito
+    // al igual que las mutaciones, el primer parámetro es el estado,
+    // además, Vuez pasa todos los getters existentes como el segundo parámetro.
+    // Los getters rastrean sus propias dependencias y se actualizan automáticamente
+    // cuando una dependencia cambia.
+    availableProducts(state, getters) {
+      return state.products.filter(product => product.inventory > 0);
+    }
+  },
+  actions: { // = métodos, aquí realizamos la llamada al api
+             // Las acciones son métodos del store
+             // caundo necesitamos ubicar los productos desde una API
+             // creamos una acción para ello
+             // Las acciones son usualmente asíncronas
+    // Vuex automáticamente pasa el objeto context como primer parámetro
+    // a todas sus acciones.
+    // El objeto context provee el mismo conjunto de métodos y propiedades
+    // que el objeto store. Esto significa que podemos usar:
+    // context.commit para activar una mutación
+    // context.state para acceder al estado
+    // Las acciones también son responsables de la lógica implícita,
+    // en la activación de una mutación
+    // fetchProducts(context) { // podemos usar la desestructuración
+                             // para solo tomar el método commit del objeto ccontext
+    fetchProducts({commit}) {
+      // make the call
+      // then run setProducts mutations
+      // Las acciones son usualmente asíncronas necesitamos saber cuando
+      // se realiza una acción,para ello, podemos devolver una promesa de nuestra
+      // acción y manejarla luego con store.dispatch
+      return new Promise((resolve, reject) => {
+        shop.getProducts(products => {
+          // store.commit('setProducts', products);
+          //context.commit('setProducts', products);
+          commit('setProducts', products)
+          resolve()
+        });
+      })
+    },
+    // las acciones es responsables por la lógica implicada en la activación
+    // de una mutación
+    addProductToCart(context, product) {
+      if(product.inventory > 0) {
+        const cartItme = context.state.cart.find(item => item.id === product.id);
+        if(!cartItem) {
+          // si el item no existe lo añadiremos al carrito
+          context.commit('pushProductToCart', product.id);
+        }
+      } else {
+        // Si el cartItem existe, haremos commit de otra mutación
+        context.commit('incrementItemQuantity', cartItem);
+      }
+      context.commit('decrementProductInventory', product);
+    }
+  },
+  mutations: { // responsables de establecer y actualizar el estado
+    // Las mutationes son responsables de cambios de estado individuales.
+    // Un cambio de estado individual podría ser actualizar
+    // el arreglo products en nuestro store
+    // queremos establecer el array products, pues tenemos que llamar a
+    // setProducts
+    // Vuex automáticamente pasará el estado como primer parámetro,
+    // podemos pasar un parámetro adicional, al llamar a una mutación
+    // que será la carga (En este ejemplo es productos)
+    // el propósito de la mutaciones no es obtener datos
+    // o hacer cálculos complejos sino alterar el estado
+    setProducts(state, products) {
+      // update products
+      state.products = products; // actualizamos el estado de products en la carga
+    }
+  }
+});
+
